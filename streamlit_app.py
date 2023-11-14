68d2e73f6c68b1fb782b657e2a994e6705c4b819
mangeshw2000 <mangeshw2000@yahoo.com>
2023-11-14 16:36:13 +0530

    Update streamlit_app.py
    Added header & text.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit
 streamlit.title('My Parent new healthy dinner')
-streamlit.title('Breakfast Menu')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
