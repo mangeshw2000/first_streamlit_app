f2c90bae10fb27b4a31545a9703cfa3d4903adf8
mangeshw2000 <mangeshw2000@yahoo.com>
2023-11-14 16:40:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title('My Parent new healthy dinner')
-streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
